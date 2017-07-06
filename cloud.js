86c118c287e65003bec63dc5c212ffb03cc8df6f
yxd <qweqwe_40343@163.com>
2017-07-06 22:50:56 +1000

    test

--- cloud.js
+++ cloud.js
@@ -3,8 +3,8 @@ var AV = require('leanengine');
 /**
  * 一个简单的云代码方法
  */
-AV.Cloud.define('hello', function(request) {
-  return 'Hello world!';
+AV.Cloud.define('hello', function (request) {
+    return 'Hello world!';
 });
 //constant
 var quizSize = 50;
@@ -14,37 +14,63 @@ var randomSort = function (a, b) {
     return Math.random() > .5 ? -1 : 1;
 };
 //shuffling function
-var shuffle = function(total){
-        var retVal = [];
-        for (var i = 1; i <= total; i++) {
-            retVal.push(i);
-        }
-        retVal = retVal.sort(randomSort);
-        retVal = retVal.slice(0, quizSize);
-        return retVal;
+var shuffle = function (total) {
+    var retVal = [];
+    for (var i = 1; i <= total; i++) {
+        retVal.push(i);
+    }
+    retVal = retVal.sort(randomSort);
+    retVal = retVal.slice(0, quizSize);
+    return retVal;
 };
 
 //acquire questions from cloud
-AV.Cloud.define('randomAcquireQuestions',function(request){
-	var level = request.params.level;
-	var query = new AV.Query("config");
-	query.equalTo("level",level);
-	return query.find().then(function(results){
-		var number = results[0].get("number");
-		var arr = shuffle(number);
+AV.Cloud.define('randomAcquireQuestions', function (request) {
+    //get level info
+    var level = request.params.level;
+    var query = new AV.Query("config");
+    query.equalTo("level", level);
+    return query.find().then(function (results) {
+        //randomly acquire questions from the target question set
+        var number = results[0].get("number");
+        var arr = shuffle(number);
+        //query
+        var mainQuery = new AV.Query(level);
+        mainQuery.equalTo("id", arr[0]);
+        for (var i = 1; i < quizSize; i++) {
+            var query = new AV.Query(level);
+            query.equalTo("id", arr[i]);
+            mainQuery = AV.Query.or(mainQuery, query);
+        }
+        return mainQuery.find().then(function (results) {
+            return results.sort(randomSort);
+        });
+    });
+});
 
-		var mainQuery = new AV.Query(level);
-		mainQuery.equalTo("id",arr[0]);
-		
-		for(var i = 1;i<quizSize;i++){
-			var query = new AV.Query(level);
-			query.equalTo("id",arr[i]);
-			mainQuery = AV.Query.or(mainQuery,query);
-		}
-		
-		return mainQuery.find().then(function(results){
-			return results.sort(randomSort);
-		});
-		
-	});
+AV.Cloud.define('uploadscore', function (request) {
+    var level = request.params.level;
+    var score = request.params.score;
+    var time = request.params.time;
+    var user = request.params.user;
+    var LB = AV.Object.extend('leaderboard');
+    //
+
+
+    var entry = new LB();
+    entry.set('lvl', level);
+    entry.set('score', score);
+    entry.set('user', user);
+    entry.set('exactTime', time);
+    entry.set('uploadTime', new Date());
+    return entry.save().then(function (savedEntry) {
+        //console.log('objectId is ' + todo.id);
+        var query = new AV.Query("LeaderBoard");
+        query.equalTo("level",this.level);
+        return this.level;
+    }, function (error) {
+        console.error(error);
+        return error;
+    });
+
 });
