2f31ce13d5ba164bc57c42ef4a001c06bea38cb1
yxdivine <xinda.yu@outlook.com>
2017-07-21 18:55:13 +1000

    Update cloud.js

--- cloud.js
+++ cloud.js
@@ -132,7 +132,7 @@ AV.Cloud.define('uploadRecord', function (request) {
             entry = success[0];
             var cscore = entry.get(level + "_score");
             var ctime = entry.get(level + "_time");
-            console.log(cscore + "|" + ctime);
+            console.log(JSON.stringify(entry));
             if (cscore && (cscore > score || (cscore == score && ctime < time))) {
                 entry.set(level + "_score", score);
                 entry.set(level + "_time", time);
